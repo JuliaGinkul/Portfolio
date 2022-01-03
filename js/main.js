de50439285b8e631b380df71999603d42cb262db
JuliaGinkul <ginkul18@gmail.com>
2022-01-03 16:27:51 +0300

    add js and GSAP animation on the main page

--- js/main.js
+++ js/main.js
@@ -33,9 +33,51 @@ $(document).ready(function(){
 });
 
 let tl = gsap.timeline();
-tl.from('.letter', {
-    duration: 1,
-    opacity: 1,
-    stagger: 0.25,
-    color: '#ffdf21',  
+tl.from('.header-title', {
+    duration: 0.6,
+    opacity: 0,
+    y: 200, 
+
 });
+tl.from('.header-subtitle', {
+    duration: 0.6,
+    y: 200,
+    opacity: 0,
+});
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+               if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+               }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 600);
+}
